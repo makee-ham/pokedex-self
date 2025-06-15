c021378d7aec4c45761f6893bcd4ff1aee105171
Makee <ina0312@naver.com>
2025-06-15 23:30:33 +0900

    #6 분명 다 맞는데 왜 안 되지 했는데 컴포넌트 안 불러온 거였다 아무튼 찜목록도 구현했다

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,13 +1,19 @@
+import Cards from "../components/Cards";
 import useFavorite from "../hooks/useFavorite";
+import usePokemonDataStore from "../hooks/usePokemonDataStore";
 
 export default function Favorites() {
   const { favoriteIds } = useFavorite();
+  const { pokemons } = usePokemonDataStore();
+  const favoritePokemons = pokemons.filter(
+    (p) => favoriteIds.includes(p.id) === true
+  );
+
   return (
     <div className="flex flex-wrap justify-center gap-10">
-      {favoriteIds}
-      {/* {pokemons.map((pokemon) => (
+      {favoritePokemons.map((pokemon) => (
         <Cards key={pokemon.id} data={pokemon} />
-      ))} */}
+      ))}
     </div>
   );
 }
